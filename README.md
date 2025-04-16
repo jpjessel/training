586dd06812007636bbf07c95339ed34af5b0e214
Jack Jessel <jack.jessel@lcp.uk.com>
2025-04-16 16:59:38 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# training
-A repo for my colleagues and their python training 
+# Training Modules
+
+## Session 1
+
+### Single Responsbility Principle
+A SOLID Principle, "A class should have one, and only one, reason to change."
+
+### Dry code
+Every piece of knowledge must have a single, unambiguous, authoritative representation within a system.
+
+### KISS
+Keep it simple stupid, most systems work best if they are kept simple, rather than made complicated.
---
+++ session_1/example.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def process_data(filename, file_nameprefix, date_columns_name, category_column_name, value_column_name):
+    df = pd.read_csv(file_nameprefix + filename)
+
+    df = df.dropna()
+    df[date_columns_name] = pd.to_datetime(df[date_columns_name])
+    
+    if summary == "mean":
+        summary = df.groupby(category_column_name)[value_column_name].mean()
+    elif summary == "median":
+        summary = df.groupby(category_column_name)[value_column_name].median()
+
+    print("Summary Stats:")
+    print(summary)
+
+    summary.to_csv("output/summary.csv")
+
+def main(file_path):
+    process_data(file_path)
+
+if __name__ == "__main__":
+    main()
